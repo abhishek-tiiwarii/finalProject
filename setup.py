dab37b5777751822f98c111a3e093378d92749e1
abhishek-tiiwarii <abhishektiiwarii7@gmail.com>
2025-04-13 18:36:44 +0530

    Commit1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Cura',
+    version= '0.0.0',
+    author= 'Abhishek Tiwari',
+    author_email= 'abhishektiiwarii7@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
